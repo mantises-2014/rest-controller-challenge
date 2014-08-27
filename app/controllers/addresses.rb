3a3002a703fcab720dcf3e1b9db6b0336373fbaa
Dev & Pete Brooks <pair+ddeol77.petebrooks@devbootcamp.com>
2014-08-27 10:14:21 -0500

    challenge complete

--- app/controllers/addresses.rb
+++ app/controllers/addresses.rb
@@ -1,4 +1,25 @@
-# get '/contacts/:contact_id' do
+get '/contacts/:id/addresses/new' do
+  @contact = Contact.find(params[:id])
+  @address = Address.new
+  erb :'addresses/new'
+end
 
-#   # erb :"contacts/show"
-# end
+get '/contacts/:id/addresses/:address_id/edit' do
+  @contact = Contact.find(params[:id])
+  @address = Address.find(params[:address_id])
+  erb :'addresses/edit'
+end
+
+post '/contacts/:id/addresses' do
+  @contact = Contact.find(params[:id])
+  @contact.addresses.create(params[:address])
+  redirect "/contacts/#{@contact.id}"
+end
+
+post '/contacts/:id/addresses/:address_id' do
+  @contact = Contact.find(params[:id])
+  @address = Address.find(params[:address_id])
+  @address.update(params[:address])
+  redirect "/contacts/#{@contact.id}"
+end
+
--- app/controllers/contacts.rb
+++ app/controllers/contacts.rb
@@ -4,17 +4,39 @@ get '/contacts' do
 end
 
 get '/contacts/new' do
-  @contact = Contact.create
+  @contact = Contact.new
   erb :'contacts/new'
-end
-
-post '/contacts' do
-  @contact = Contact.create(params[:contacts])
-  @contact.save
-  erb :'contacts/index'
 end
 
 get '/contacts/:id/edit' do
   @contact = Contact.find(params[:id])
   erb :'contacts/edit'
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  erb :'contacts/show'
+end
+
+# ===========================================================
+
+# Update contact
+post '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  @contact.update(params[:contact])
+  redirect "/contacts/#{@contact.id}"
+end
+
+# Delete Contact
+post '/contacts/:id/delete' do
+  Contact.find(params[:id]).destroy
+  redirect "/contacts"
+end
+
+# Create contact
+post '/contacts' do
+  @contact = Contact.create(params[:contact])
+  @contact.save
+  @contacts = Contact.all
+  erb :'contacts/show'
 end
