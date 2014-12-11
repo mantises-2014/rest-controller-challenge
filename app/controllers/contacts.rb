5f752c64c996344319386c634d8727512fa86281
Christopher Hirsh <christopherjhirsh@gmail.com>
2014-12-11 15:28:06 -0600

    finished. all tests pass.

--- app/controllers/contacts.rb
+++ app/controllers/contacts.rb
@@ -11,44 +11,34 @@ end
 
 # CREATE
 post '/contacts' do
-  # 1. read the data out of params
-  # 2. create a new Dog instance with the data
   @contact = Contact.new(params[:contact])
-
-  # 3. save dog
   @contact.save
-    # 4. redirect to show action for the new dog
-    # redirect "/dogs/#{@dog.id}"
-
-    # 5. re-render the new page with the dog's info in the form
     erb :'contacts/show'
-
 end
 
 # EDIT
 get '/contacts/:id/edit' do
-  # 1. find the dog from db
   @contact = Contact.find(params[:id])
-
-  # 2. render the form with the dog's current attributes
   erb :'contacts/edit'
 end
 
 # UPDATE
 put '/contacts/:id' do
-  # 0. find the damn dog
   @contact = Contact.find(params[:id])
-
-  # 1. gather the data from params
-  # 2. update the dog's db record
   @contact.update_attributes(params[:contact])
-
-  # 3. redirect to dog's show page
   redirect "/contacts/#{@contact.id}"
 end
 
 get '/contacts/:id' do
   @contact = Contact.find(params[:id])
   erb :'contacts/show'
+end
+
+
+# DELETE
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  @contact.destroy
+  redirect "/contacts"
 end
 
